ab2c1afdf1e7fb1012d67a157c32f208fe326dde
John Robinson <johnarobinson133@gmail.com>
2021-01-21 10:55:45 -0700

    fixed routes and server links

---
+++ routes/api.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        {_id: req.params.id},
+        {$inc: {totalDuration: req.body.duration}, $push: {exercises: req.body}},
+        {new: true}
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
-const apiRoutes = require("./workouts");
-const homeRoutes = require("./homeRoutes");
+const path = require("path");
 
-router.use("/api", apiRoutes);
 
-router.use("/", homeRoutes);
-
-router.use((req, res) => {
-    res.send("<p>not a route.</p>")
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 
 module.exports = router;
